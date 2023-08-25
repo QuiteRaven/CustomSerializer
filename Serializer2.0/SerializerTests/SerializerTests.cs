526945bb2c3fce896d14b5a43527c34713d49f67
quite <quite@DESKTOP-NA3RNC8>
2023-08-25 14:15:06 +0700

    updating and adding new tests

--- Serializer2.0/SerializerTests/SerializerTests.cs
+++ Serializer2.0/SerializerTests/SerializerTests.cs
@@ -27,6 +27,7 @@ namespace UnitTests
 
             // Assert
             AssertLinkedNodesEqual(node1, deepCopy);
+            Assert.NotSame(node1, deepCopy);
         }
 
         [Fact]
@@ -48,7 +49,7 @@ namespace UnitTests
         }
 
         [Fact]
-        public async Task SerializeDeserialize_EmptyList_ShouldThrowException()
+        public async Task SerializeDeserialize_EmptyList_ShouldReturnNull()
         {
             // Arrange
             var emptyList = null as ListNode;
@@ -79,27 +80,131 @@ namespace UnitTests
             }
         }
 
+        [Fact]
+        public async Task SerializeDeserialize_TwoNodes()
+        {
+            var node1 = new ListNode { Data = "Node 1" };
+            var node2 = new ListNode { Data = "Node 2" };
+
+            node1.Next = node2;
+            node2.Previous = node1;
+
+            var serializer = new ListSerializer();
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await serializer.Serialize(node1, memoryStream);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                var deserializedNode = await serializer.Deserialize(memoryStream);
+
+                AssertLinkedNodesEqual(node1, deserializedNode);
+            }
+        }
+
+        [Fact]
+        public async Task SerializeDeserialize_ThreeNodes()
+        {
+            var node1 = new ListNode { Data = "Node 1" };
+            var node2 = new ListNode { Data = "Node 2" };
+            var node3 = new ListNode { Data = "Node 3" };
+
+            node1.Next = node2;
+            node2.Next = node3;
+            node2.Previous = node1;
+            node3.Previous = node2;
+
+            var serializer = new ListSerializer();
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await serializer.Serialize(node1, memoryStream);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                var deserializedNode = await serializer.Deserialize(memoryStream);
+
+                AssertLinkedNodesEqual(node1, deserializedNode);
+            }
+        }
+
+        [Fact]
+        public async Task SerializeDeserialize_CorrectRanadom()
+        {
+            var node1 = new ListNode { Data = "Node 1" };
+            var node2 = new ListNode { Data = "Node 2" };
+            var node3 = new ListNode { Data = "Node 3" };
+
+            node1.Next = node2;
+            node2.Next = node3;
+            node2.Previous = node1;
+            node3.Previous = node2;
+            node1.Random = node3;
+            node2.Random = node1;
+            node3.Random = node2;
+
+            var serializer = new ListSerializer();
+
+            using (var memoryStream = new MemoryStream())
+            {
+                await serializer.Serialize(node1, memoryStream);
+                memoryStream.Seek(0, SeekOrigin.Begin);
+                var deserializedNode = await serializer.Deserialize(memoryStream);
+
+                AssertLinkedNodesEqual(node1, deserializedNode);
+            }
+        }
+
         private void AssertLinkedNodesEqual(ListNode expected, ListNode actual)
         {
-            var expectedNodes = new List<ListNode>();
-            var actualNodes = new List<ListNode>();
+            var nodeMap = new Dictionary<ListNode, ListNode>();
+
+            var currentExpected = expected;
+            var currentActual = actual;
+            while (currentExpected != null && currentActual != null)
+            {
+                nodeMap[currentExpected] = currentActual;
+                currentExpected = currentExpected.Next;
+                currentActual = currentActual.Next;
+            }
 
             while (expected != null && actual != null)
             {
-                expectedNodes.Add(expected);
-                actualNodes.Add(actual);
+
                 Assert.Equal(expected.Data, actual.Data);
+                
+                if (expected.Random != null)
+                {
+                    Assert.True(nodeMap.ContainsKey(expected.Random));
+                    Assert.Same(nodeMap[expected.Random], actual.Random);
+                }
+                else
+                {
+                    Assert.Null(actual.Random);
+                }
+
+
+                if (expected.Previous != null)
+                {
+                    Assert.True(nodeMap.ContainsKey(expected.Previous));
+                    Assert.Same(nodeMap[expected.Previous].Next, actual);
+                }
+                else
+                {
+                    Assert.Null(actual.Previous);
+                }
+
+                if (expected.Next != null)
+                {
+                    Assert.True(nodeMap.ContainsKey(expected.Next));
+                    Assert.Same(nodeMap[expected.Next].Previous, actual);
+                }
+                else
+                {
+                    Assert.Null(actual.Next);
+                }
+                
                 expected = expected.Next;
                 actual = actual.Next;
             }
-
             Assert.Equal(expected, actual);
-
-            for (int i = 0; i < expectedNodes.Count; i++)
-            {
-                Assert.Equal(expectedNodes[i].Previous?.Data, actualNodes[i].Previous?.Data);
-                Assert.Equal(expectedNodes[i].Next?.Data, actualNodes[i].Next?.Data);
-            }
         }
     }
 }
